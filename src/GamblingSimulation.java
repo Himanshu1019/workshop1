12db72f87a4a83ead8de8d41995e4d1514e8c9c3
classroom <himanshukanoje23@gmail.com>
2022-03-31 14:30:29 +0530

    my commit

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -9,43 +9,62 @@ public class GamblingSimulation {
 		int loss;
 		int winrate=0;
 		int totalamount=100;
-		Scanner s = new Scanner(System.in);
-	      System.out.println("Enter a no of turns" );
-	      int play = s.nextInt();
+	int wonamount=0;
+	int lostamount=0;
+		
+		for(int day=1;day<=20;day++)
+		{
+			
+		
+				Scanner s = new Scanner(System.in);
+				System.out.println("Enter a no of turns for day "+day );
+				int play = s.nextInt();
 		
 		  
-		for (int i=1;i<=play;i++)
-		{
-			win =(int) Math.floor(Math.random()*10)%2;
-			if(win==1)
+				for (int i=1;i<=play;i++)
 				{
-			winrate=winrate+win;
-			System.out.println(" gambler won this turn  "  +win);
-			totalamount=totalamount+1;
-			System.out.println(" "  +totalamount);
+					win =(int) Math.floor(Math.random()*10)%2;
+					if(win==1)
+				{
+						winrate=winrate+win;
+						System.out.println(" gambler won this turn  "  +win);
+						totalamount=totalamount+1;
+						System.out.println(" "  +totalamount);
 			
 					}
-			else if (win==0)
-				{int a=-1;
+					else if (win==0)
+				{
+						
+						int a=-1;
 			winrate=winrate-win;
 			System.out.println(" gambler looses this turn " +a);
 			totalamount=totalamount-1;
 			System.out.println(" "  +totalamount);
 				}
-		}
+				}
 		  
-		if(totalamount>=150) 
-		{
-			System.out.println("gambler won the game " +totalamount);
-		}
-		else if (totalamount<=50)	
-		{
-			System.out.println("gambler looses the game " +totalamount);
-		}
-		else
-		{
-			System.out.println("gambler needs to play more to get the result");
-		}
+				if(totalamount>=150) 
+					{
+					
+					System.out.println("gambler won the game " +totalamount);
+					 wonamount=totalamount-150;
+					 System.out.println("the amount gamler won on day "+day+ "is"  +wonamount);
+					}
+				else if (totalamount<=50)	
+					{
+					System.out.println("gambler looses the game " +totalamount);
+					lostamount=150-totalamount;
+					 System.out.println("the amount gamler lost on day "+day+ "is"  +lostamount);
+					}
+				else
+				{
+					System.out.println("gambler needs to play more to get the result");
+				}
+				
+				wonamount=wonamount+0;
+				lostamount=lostamount+0;
 	}
-
+		System.out.println("the total amount gambler won after 20 days of continous gaming is  " +wonamount);
+		System.out.println("the total amount gambler losses after 20 days of continous gaming is  " +lostamount);
+}
 }
