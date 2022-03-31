9a2c8c7e62ceab452feff45fa39dd06b419674f5
classroom <himanshukanoje23@gmail.com>
2022-03-31 09:18:04 +0530

    my commit

--- src/GamblingSimulation.java
+++ src/GamblingSimulation.java
@@ -4,22 +4,26 @@ public class GamblingSimulation {
 	int betpergame=1;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	int	win=0;
-		int loss=0;
+	int	win;
+		int loss;
 		int winrate=0;
-		  win =(int) Math.floor(Math.random()%2);
+		  win =(int) Math.floor(Math.random()*10)%2;
+		  
+		  
 		if(win==1)
 		{
 			winrate=winrate+win;
+			System.out.println(" gambler won the game "+winrate);
 		}
 		else if(win==0)
 		{
 			winrate=winrate-1;
+			System.out.println(" gambler looses the game "+winrate);
 		}
-	
+		  }
 			
-		
+	
 
-	}
+	
 
 }
